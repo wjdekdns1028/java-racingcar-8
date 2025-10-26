992fe4d32a14847f3169914d63ff9d10e787f227
daun <wjdekdns0218@gmail.com>
2025-10-26 14:29:46 +0900

    feat(controller): 전체 프로그램 흐름 제어 로직 추가 및 예외 발생 시 프로그램 종료

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,32 @@
+package racingcar.controller;
+
+import racingcar.domian.Cars;
+import racingcar.domian.Race;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class RacingController {
+
+    public void run() {
+        try {
+            // 1. 입력 처리
+            List<String> carNames = InputView.readCarNames();
+            int attemptCount = InputView.readAttemptCount();
+
+            // 2. 예외 처리
+            Cars cars = new Cars(carNames);
+
+            // 3. 경주 진행
+            Race race = new Race(cars, attemptCount);
+            race.start();
+
+            // 3. 출력
+            OutputView.printWinners(cars.findWinners());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR " + e.getMessage());
+        }
+    }
+}
