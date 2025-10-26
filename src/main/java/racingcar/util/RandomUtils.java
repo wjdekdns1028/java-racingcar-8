15e997bfef7374f1ab6b697f8368acff9661ea09
daun <wjdekdns0218@gmail.com>
2025-10-26 13:47:26 +0900

    feat(util): Randoms.pickNumberInRange() 기반 랜덤값 생성 기능 추가

---
+++ src/main/java/racingcar/util/RandomUtils.java
@@ -0,0 +1,10 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomUtils {
+
+    public static int pickNumberInRange(int start, int end) {
+        return Randoms.pickNumberInRange(start, end);
+    }
+}
